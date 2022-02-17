f4e8304cd912119bbd52d82717f83d1766ca8540
bmsani <bmsani6@gmail.com>
2022-02-17 21:19:33 +0600

    function added

--- app.js
+++ app.js
@@ -1,43 +1,51 @@
+function inputIdCaller(idName) {
+  const inputValue = document.getElementById(idName);
+  const valueToNumber = parseInt(inputValue.value);
+  return valueToNumber;
+}
+
+function innerTextChanger(idName, changeData) {
+  const textFeild = document.getElementById(idName);
+  textFeild.innerText = changeData;
+}
+
 document.getElementById("calculate").addEventListener("click", function () {
-    const totalIncome = document.getElementById('total-income');
-    const totalIncomeNumber = parseInt(totalIncome.value);
+  const totalIncome = inputIdCaller("total-income");
+  const foodCostNumber = inputIdCaller("food-cost");
+  const rentCostNumber = inputIdCaller("rent-cost");
+  const clothCostNumber = inputIdCaller("cloth-cost");
+
+  const totalCost = foodCostNumber + rentCostNumber + clothCostNumber;
+
+  const balance = totalIncome - totalCost;
+
+  if (
+    totalIncome > 0 &&
+    foodCostNumber > 0 &&
+    rentCostNumber > 0 &&
+    clothCostNumber > 0
+  ) {
+    innerTextChanger("total-expenses", totalCost);
+    innerTextChanger("balance", balance);
+  }else{
     
-    const foodCost = document.getElementById('food-cost');
-    const foodCostNumber = parseInt(foodCost.value);
-
-    const rentCost = document.getElementById('rent-cost');
-    const rentCostNumber = parseInt(rentCost.value)
-
-
-    const clothCost = document.getElementById('cloth-cost');
-    const clothCostNumber = parseInt(clothCost.value)
-
-    const totalCost = foodCostNumber + rentCostNumber + clothCostNumber;
-
-    const balance = totalIncomeNumber - totalCost;
-
-    const totalExpensesFeild = document.getElementById('total-expenses');
-    totalExpensesFeild.innerText = totalCost;
-    const balanceFeild = document.getElementById('balance');
-     balanceFeild.innerText = balance;
-
-  console.log(totalIncomeNumber,foodCostNumber,rentCostNumber,clothCostNumber, totalCost, balance);
+  }
 });
 
 document.getElementById("saving").addEventListener("click", function () {
-    const savingRate = document.getElementById('saving-percentage');
-    const savingRateNumber = parseInt(savingRate.value)
+  const savingRate = document.getElementById("saving-percentage");
+  const savingRateNumber = parseInt(savingRate.value);
 
-    const balanceFeild = document.getElementById('balance');
-    const balanceFeildNumber = parseInt(balanceFeild.innerText);
+  const balanceFeild = document.getElementById("balance");
+  const balanceFeildNumber = parseInt(balanceFeild.innerText);
 
-    const totalSaving =  (balanceFeildNumber * savingRateNumber) / 100;
+  const totalSaving = (balanceFeildNumber * savingRateNumber) / 100;
 
-    const savingAmount = document.getElementById('saving-amount');
-    savingAmount.innerText = totalSaving;
+  const savingAmount = document.getElementById("saving-amount");
+  savingAmount.innerText = totalSaving;
 
-    const remainingBalance = document.getElementById('remaining-balance');
-    remainingBalance.innerText = balanceFeildNumber - totalSaving;
+  const remainingBalance = document.getElementById("remaining-balance");
+  remainingBalance.innerText = balanceFeildNumber - totalSaving;
 
   console.log(remainingBalance.innerText);
 });
